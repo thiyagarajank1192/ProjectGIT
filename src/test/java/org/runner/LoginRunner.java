840d7f4c0573ecf44701bc37dea1353d3d678293
thiyagarajank1192 <thiyagarajank1192@gmail.com>
2024-10-30 12:28:35 +0530

    empty code

--- src/test/java/org/runner/LoginRunner.java
+++ src/test/java/org/runner/LoginRunner.java
@@ -5,18 +5,3 @@ import org.junit.runner.RunWith;
 import org.utilities.JVMReport;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources",
-				 glue = "org.stepdefinition",
-				 dryRun = false,
-				 tags= {"@Login"},
-				 plugin= {"json:target\\Reports\\login.json" })
-
-public class LoginRunner {
-	@AfterClass
-	public static void methid() {
-		JVMReport r=new JVMReport();
-		r.generateReport(System.getProperty("user.dir")+"\\target\\Reports\\login.json");	
-	}
-	
-}
